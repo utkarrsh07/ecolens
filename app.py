a30a05d2de3b27aaadd26246050cddc66ba888f8
Utkarsh Singh <singh.utkarsh.ca@gmail.com>
2026-03-23 15:31:38 -0400

    Update app for Render port

--- app.py
+++ app.py
@@ -402,5 +402,8 @@ def home():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
